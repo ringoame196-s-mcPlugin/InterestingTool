1dfa145811bff1f7542b767ee147e325010254cb
りんご飴196 <kazumac41@gmail.com>
2025-03-17 14:28:37 +0900

    ✨ 右クリック 左クリックイベント実装

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/ToolEvents.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/ToolEvents.kt
@@ -1,5 +1,37 @@
 package com.github.ringoame196_s_mcPlugin.events
 
+import com.github.ringoame196_s_mcPlugin.Data
+import com.github.ringoame196_s_mcPlugin.events.Interface.LeftClick
+import com.github.ringoame196_s_mcPlugin.events.Interface.RightClick
+import com.github.ringoame196_s_mcPlugin.managers.InterestingToolManager
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.ItemStack
 
-class ToolEvents : Listener
+class ToolEvents : Listener {
+    private val interestingToolManager = InterestingToolManager()
+
+    private fun acquisitionInterToolId(item: ItemStack): String? {
+        return interestingToolManager.acquisitionInterToolId(item)
+    }
+
+    @EventHandler
+    fun onRightClick(e: PlayerInteractEvent) {
+        if (e.action != Action.RIGHT_CLICK_AIR && e.action != Action.RIGHT_CLICK_BLOCK) return
+        val interToolId = acquisitionInterToolId(e.item ?: return) ?: return
+        val interestingTool = Data.InterestingToolMap[interToolId] ?: return
+        if (interestingTool !is RightClick) return
+        interestingTool.rightClick(e)
+    }
+
+    @EventHandler
+    fun onLeftClick(e: PlayerInteractEvent) {
+        if (e.action != Action.RIGHT_CLICK_AIR && e.action != Action.RIGHT_CLICK_BLOCK) return
+        val interToolId = acquisitionInterToolId(e.item ?: return)
+        val interestingTool = Data.InterestingToolMap[interToolId] ?: return
+        if (interestingTool !is LeftClick) return
+        interestingTool.leftClick(e)
+    }
+}
