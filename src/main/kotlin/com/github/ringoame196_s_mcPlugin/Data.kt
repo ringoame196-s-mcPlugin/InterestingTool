937cf83fdfac64d2b6a3efc1dc83b7be636e96c4
りんご飴196 <kazumac41@gmail.com>
2025-03-17 14:09:03 +0900

    ✨ データ関係

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Data.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Data.kt
@@ -1,4 +1,11 @@
 package com.github.ringoame196_s_mcPlugin
 
+import com.github.ringoame196_s_mcPlugin.tools.InterestingTool
+import com.github.ringoame196_s_mcPlugin.tools.SnowBallShovel
+
 object Data {
+    val interestingToolList = listOf(
+        SnowBallShovel()
+    )
+    val InterestingToolMap = mutableMapOf<String, InterestingTool>()
 }
--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -1,14 +1,28 @@
 package com.github.ringoame196_s_mcPlugin
 
-import org.bukkit.plugin.java.JavaPlugin
 import com.github.ringoame196_s_mcPlugin.commands.Command
 import com.github.ringoame196_s_mcPlugin.events.ToolEvents
+import com.github.ringoame196_s_mcPlugin.events.ToolListGUIEvents
+import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
-    private val plugin = this
+
+    companion object {
+        lateinit var plugin: Main
+            private set
+    }
+
     override fun onEnable() {
         super.onEnable()
+        plugin = this
+
+        // map登録
+        for (interestingTool in Data.interestingToolList) {
+            Data.InterestingToolMap[interestingTool.interToolId] = interestingTool
+        }
+
         server.pluginManager.registerEvents(ToolEvents(), plugin)
+        server.pluginManager.registerEvents(ToolListGUIEvents(), plugin)
         val command = getCommand("intertool")
         command!!.setExecutor(Command())
     }
